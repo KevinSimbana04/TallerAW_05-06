5b59fb29dfd6622bff6465192731b73485d7556d
KevinS04 <kevinsimbana2004@outlook.com>
2025-10-18 19:20:54 -0500

    Formato Taller

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="container">
+      <header>
+        <h1>Laboratorio 6</h1>
+        <p>Desarrollo de Aplicaciones Web - Escuela Politécnica Nacional</p>
+      </header>
+
+      <section>
+        <h2>Mercy</h2>
+      </section>
+
+      <section>
+        <h2>Kevin</h2>
+
+      </section>
+
+      <section>
+        <h2>Joel</h2>
+      </section>
+
+      <footer>
+        <table border="1">
+          <thead>
+            <tr>
+              <th>Repositorio</th>
+              <th>Despliegue</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><a href="https://github.com/KevinSimbana04/Taller05-06_AW">Taller 05-06</a></td>
+              <td><a href="#">Netlify</a></td>
+            </tr>
+          </tbody>
+        </table>
+      </footer>
+    </div>
+  );
+}
+
+export default App
